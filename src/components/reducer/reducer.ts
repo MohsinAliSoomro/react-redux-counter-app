386b6af03634aa20b4f15954e10b4b139e04646c
MohsinAliSoomro <mohsinalisoomro23@gmail.com>
2020-09-18 11:19:56 +0500

    install redux and @types/react-redux and make store and reducer as well

---
+++ src/components/reducer/reducer.ts
@@ -0,0 +1,18 @@
+const initialState={
+    count:0
+}
+
+export function reducer(state = initialState,action:any){
+    switch (action.type) {
+        case "increment":
+            return {
+                ...state,count:state.count+1
+            }
+        case "decrement":
+            return {
+                ...state, count: state.count - 1
+            }
+        default:
+            return state;
+    }
+}
